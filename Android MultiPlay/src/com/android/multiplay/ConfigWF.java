eedbdbe6c20e5c422a992fb2de5d666bf334154b
PWrGitHub194244 <194244@student.pwr.wroc.pl>
2013-10-31 21:16:09 +0100

    ?

---
+++ Android MultiPlay/src/com/android/multiplay/ConfigWF.java
@@ -0,0 +1,22 @@
+package com.android.multiplay;
+
+public class ConfigWF {
+	private String  ip;
+	private int port;
+	 public void setip(String ip){
+		this.ip=ip;
+	}
+	 public void setport(int port){
+			this.port=port;
+		}
+
+	public int getport() {
+		return port;
+	}
+
+	public String getip() {
+		return ip;
+	}
+	 
+
+}

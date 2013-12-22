f0962e4e08cf37705e321c25aa741cac0319ba53
PWrGitHub194244 <baczkiewiczpiotr@gmail.com>
2013-12-22 16:55:57 +0100

    connection

--- MultiPlay Server/src/Frame/Main.java
+++ MultiPlay Server/src/Frame/Main.java
@@ -20,37 +20,37 @@ public class Main {
 		window.setVisible(true);
 		int port = 1234;
 		byte data = 0;
-		ServerSocket serversocket;
-		try {
-			serversocket = new ServerSocket(port);
+		while (true) {
+			ServerSocket serversocket;
 			try {
-				Socket socket = serversocket.accept();
-				dis = new DataInputStream(socket.getInputStream());
-				dos = new DataOutputStream(socket.getOutputStream());
-				System.out.println("RUN");
-				// TODO Auto-generated method stub
-
-				System.out.println("Read...");
-				data = dis.readByte();
-				if (data == N.signal.need_authorization) {
-					System.out.println("Send back authorization code...");
-					dos.writeByte(N.signal.need_authorization);
-					System.out.println("Over");
-					Serverwifi wifi = new Serverwifi(socket, dis, dos);
-					wifi.run();
-				} else if (data == N.signal.need_to_connect) {
-					System.out.println("Send back connect code...");
-					dos.writeByte(N.signal.need_to_connect);
-
+				serversocket = new ServerSocket(port);
+				try {
+					Socket socket = serversocket.accept();
+					dis = new DataInputStream(socket.getInputStream());
+					dos = new DataOutputStream(socket.getOutputStream());
+					System.out.println("RUN");
+					System.out.println("Read...");
+					data = dis.readByte();
+					if (data == N.signal.need_authorization) {
+						System.out.println("Send back authorization code...");
+						dos.writeByte(N.signal.need_authorization);
+						System.out.println("Over");
+						dis.close();
+						dos.close();
+						socket.close();
+					} else if (data == N.signal.need_to_connect) {
+						System.out.println("Send back connect code...");
+						dos.writeByte(N.signal.need_to_connect);
+						Serverwifi wifi = new Serverwifi(socket, dis, dos);
+						wifi.run();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-
-				e.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
 
 	}

952a9fe2633de42e38e2a86592dd5ecb408c9cfd
PWrGitHub194244 <194244@student.pwr.wroc.pl>
2013-12-25 10:35:46 +0100

    ograniczenie zeby myszka nie wychodziła poza ekran

--- MultiPlay Server/src/Mouse/Mouse.java
+++ MultiPlay Server/src/Mouse/Mouse.java
@@ -1,10 +1,12 @@
 package Mouse;
 
 import java.awt.AWTException;
+import java.awt.Dimension;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.PointerInfo;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
 
@@ -17,7 +19,11 @@ public class Mouse {
 			Point b = a.getLocation();
 			int x1 = (int) b.getX();
 			int y1 = (int) b.getY();
-			robot.mouseMove(x1 + x, y1 + y);
+			Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+			if (!(x1 + x > d.getWidth()))
+				robot.mouseMove(x1 + x, y1);
+			if (!(y1 + y > d.getHeight()))
+				robot.mouseMove(x1, y1 + y);
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

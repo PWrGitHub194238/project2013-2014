c5dc53a1cc10bb9c1d5430c53b8b24a797289285
PWrGitHub194244 <194244@student.pwr.wroc.pl>
2013-12-26 21:47:31 +0100

    add imagebutton

--- Android MultiPlay/src/com/android/controllers/keyboard/Speaker.java
+++ Android MultiPlay/src/com/android/controllers/keyboard/Speaker.java
@@ -22,21 +22,17 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Speaker extends Activity  {
-	   private Button btnSpeak;
-	    private TextView txtText;
-	    protected static final int RESULT_SPEECH = 1;
+public class Speaker extends Activity {
+	private ImageButton btnSpeak;
+	//private TextView txtText;
+	protected static final int RESULT_SPEECH = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_speaker);
-		
-		  txtText = (TextView) findViewById(R.id.text2);
-		  
-	        btnSpeak = (Button) findViewById(R.id.button1);
-	 
-	    
-	 
+	//	txtText = (TextView) findViewById(R.id.text2);
+		btnSpeak = (ImageButton) findViewById(R.id.button1);
 	}
 
 	@Override
@@ -46,50 +42,42 @@ public class Speaker extends Activity  {
 		return true;
 	}
 
-	
-
 	public void onClick(View arg0) {
-		 Intent intent = new Intent(
-                 RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
-         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "pl-PL");
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "pl-PL");
 
-         try {
-             startActivityForResult(intent, RESULT_SPEECH);
-             txtText.setText("");
-         } catch (ActivityNotFoundException a) {
-             Toast t = Toast.makeText(getApplicationContext(),
-                     "Opps! Your device doesn't support Speech to Text",
-                     Toast.LENGTH_SHORT);
-             t.show();
-         }
-		
-		
+		try {
+			startActivityForResult(intent, RESULT_SPEECH);
+			//txtText.setText("");
+		} catch (ActivityNotFoundException a) {
+			Toast t = Toast.makeText(getApplicationContext(),
+					"Opps! Your device doesn't support Speech to Text",
+					Toast.LENGTH_SHORT);
+			t.show();
+		}
 	}
-	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	        super.onActivityResult(requestCode, resultCode, data);
-	 
-	        switch (requestCode) {
-	        case RESULT_SPEECH: {
-	            if (resultCode == RESULT_OK && null != data) {
-	 
-	                ArrayList<String> text = data
-	                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-	 
-	                txtText.setText(text.get(0));
-	                int i=0;
-	                while(i<txtText.getText().length()){
-	                	Toast t = Toast.makeText(getApplicationContext(),
-	                            txtText.getText().subSequence(i, i+1),
-	                            Toast.LENGTH_SHORT);
-	                    t.show();
-	                    i++;
-	                }
-	            }
-	            break;
-	        }
-	 
-	        }
-	    }
 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case RESULT_SPEECH:
+			if (resultCode == RESULT_OK && null != data) {
+
+				ArrayList<String> text = data
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				//txtText.setText(text.get(0));
+				int i = 0;
+				while (i < text.get(0).length()) {
+					Toast t = Toast.makeText(getApplicationContext(),
+							text.get(0).subSequence(i, i + 1),
+							Toast.LENGTH_SHORT);
+					t.show();
+					i++;
+				}
+			}
+			break;
+		}
+	}
 }
